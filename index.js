aa0397f9c2336b48668a2bdcc5a596ba7f876c0e
endytang <endytang0310@gmail.com>
2023-01-23 22:25:18 +0000

    Add click function for grids

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hi")
+const player = document.querySelector('[data-player]')
+const blocks = [...document.getElementsByClassName('block')]
+
+blocks.forEach(block => {
+  block.addEventListener('click', (event) => {
+
+    // const player = document.getElementById("box1")
+    const dataPlayer = player.getAttribute('data-player')
+    if (dataPlayer == 'o') {
+      event.target.innerText = 'o'
+      player.setAttribute('data-player', 'x')
+
+    } else {
+      event.target.innerText = 'x'
+      player.setAttribute('data-player', 'o')
+
+    }
+  })
+})
+
+const resetButton = document.getElementById('resetButton')
+
+resetButton.addEventListener('click', () => {
+  player.setAttribute('data-player', 'o')
+  blocks.forEach(block => {
+    block.innerText = ''
+  })
+})
+
